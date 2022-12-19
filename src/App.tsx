1e4e1f2da3539e2a9aa25cad962e2416a6eb89b7
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-12-19 18:06:44 +0100

    Started working

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import Search from './components/Search';
 
 function App() {
   return (
   <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
-    
+      <Search />    
   </main>
   );
 }
---
+++ src/components/Search.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+const Search = () => {
+    return (
+        <section className="w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-ls rounded drop-shadow-lg text-zinc-700">
+
+hello worlf
+        </section>
+    )
+}
+
+export default Search
