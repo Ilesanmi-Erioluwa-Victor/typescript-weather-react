b92f565c0be2c6ce28af17f6d4146e9c06671dbe
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-12-19 17:53:18 +0100

    Working on App file

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-       <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
+  <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
+    
+  </main>
   );
 }
 
