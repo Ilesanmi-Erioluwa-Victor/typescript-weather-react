b422e73b3379f4a8fb5f7c509d3a999ce38babf6
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-12-20 01:01:34 +0100

    Experiencing weird bug

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,25 @@ import React, { ChangeEvent, useState } from 'react';
 // import Search from './components/Search';
 
 const App = (): JSX.Element => {
-  const [term, setTerm ] = useState("") ;
+  const [term, setTerm] = useState<String>("");
 
-  const onInputchange = (e:ChangeEvent<HTMLInputElement>)=> {
+  const onInputchange = (e: ChangeEvent<HTMLInputElement>) => {
     setTerm(e.target.value)
-   console.log(term)
+
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${term}&limit=5&appid=${ process.env.REACT_APP_API_KEY}`)
   }
   return (
-  <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
-  <section>
-    <h1>Weather <span>Forecast</span></h1>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit ea sequi, enim a natus vero?</p>
-  </section>
+    <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
+      <section>
+        <h1>Weather <span>Forecast</span></h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit ea sequi, enim a natus vero?</p>
+      </section>
 
-  <div className="flex mt-10">
-  <input type="text" name='text' value={term} onChange={onInputchange}/>
-  <button type="submit">Send</button>
-  </div>
-  </main>
+      <div className="flex mt-10">
+        <input type="text" name='text' value={term} onChange={onInputchange} />
+        <button type="submit">Send</button>
+      </div>
+    </main>
   );
 }
 
